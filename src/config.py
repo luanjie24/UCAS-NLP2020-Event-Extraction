d218207084cc36e91dd0d1829c3be3167e2a5bc2
Jay <793565406@qq.com>
2020-11-27 11:36:30 +0800

    把触发词提取器的优化器写了

--- src/config.py
+++ src/config.py
@@ -2,8 +2,9 @@ import torch
 import os, sys
 
 class Config:
-    os.chdir(sys.path[0])#这句话时防止相对路径在VSCode里用不了
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') #看有没有gpu(cuda)，没有就用cpu
+    os.chdir(sys.path[0])# 这句话时防止相对路径在VSCode里用不了
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') # 看有没有gpu(cuda)，没有就用cpu
 
     # 数据集的路径 
     dataset_raw = 'nlp/dataset/CEC-Corpus/raw corpus'
@@ -13,9 +14,16 @@ class Config:
     # 预训练模型所在路径
     bert_dir='../pretrained_model/chinese_roberta_wwm_ext_pytorch'
 
-    # 超参数
+    # 参数
     batch_size = 2 # 现在设置为2是为了测试
+    train_epochs = 10 # 现在设置为10是为了测试
     sequence_length = 256 # 每句话的长度，不到这个长度就padding
     trigger_txtractor_mid_linear_dims = 128 # 这个是触发词提取模型的线性层的输出维度
+    weight_decay = 0.0001 # optimizer的权重衰减，概念参见L2正则化
+    bert_learning_rate = 0.0000001 # bert模型的学习率
+    other_learning_rate = 0.000001 # 其他模型的学习率
+    adam_epsilon = 0.00000001 # 这个是优化器为了增加数值计算的稳定性而加到分母里的项，默认就是1e-8，可以先不改
+    warmup_proportion = 0.1 # scheduler慢热学习的比例，貌似用0.的1比较多
+    
 
 
--- src/train.py
+++ src/train.py
@@ -3,13 +3,63 @@ train.py用于训练集的训练
 
 """
 
+import torch
 from config import Config
 from model import TriggerExtractor
-import torch
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+
+
+def build_optimizer_and_scheduler(model, t_total):
+
+    # 读取模型参数，模型可能来自于存起来的模型，也可能是来自于model.py初始化的模型，所以加了个下面的判断
+    module = (
+        model.module if hasattr(model, "module") else model
+    )
+    model_param = list(module.named_parameters())
+    # print(model_param) # 不懂可以输出参数看看
+
+    # 将bert的参数和其他参数分开
+    bert_param_optimizer = []
+    other_param_optimizer = []
+    for name, para in model_param:
+        space = name.split('.')
+        if space[0] == 'bert_module':
+            bert_param_optimizer.append((name, para))
+        else:
+            other_param_optimizer.append((name, para))
+
+    # 权重衰减，通用的写法：bias和LayNorm.weight不用权重衰减，其他参数进行权重衰减。参见L2正则化
+    # 此外还要差分学习率（differential learning），bert模型和其他模型学习率不同
+    no_decay = ["bias", "LayerNorm.weight"]
+    optimizer_grouped_parameters = [
+        # bert模型
+        {"params": [p for n, p in bert_param_optimizer if not any(nd in n for nd in no_decay)],
+         "weight_decay": Config.weight_decay, 'lr': Config.bert_learning_rate},
+        {"params": [p for n, p in bert_param_optimizer if any(nd in n for nd in no_decay)],
+         "weight_decay": 0.0, 'lr': Config.bert_learning_rate},
+
+        # 其他模型
+        {"params": [p for n, p in other_param_optimizer if not any(nd in n for nd in no_decay)],
+         "weight_decay": Config.weight_decay, 'lr': Config.other_learning_rate},
+        {"params": [p for n, p in other_param_optimizer if any(nd in n for nd in no_decay)],
+         "weight_decay": 0.0, 'lr':Config.other_learning_rate},
+    ]
+
+    # transformers的AdamW是目前最新的优化器，它和pytorch的AdamW参数格式统一
+    optimizer = AdamW(optimizer_grouped_parameters, lr=Config.bert_learning_rate, eps=Config.adam_epsilon)
+
+    # 学习率预热，训练时先从小的学习率开始训练 Learning Rate Scheduler，t_total是总步数
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=int(Config.warmup_proportion * t_total), num_training_steps=t_total
+    )
+
+    return optimizer, scheduler
+
 
 
 if __name__ == '__main__':
-
+ 
     # 读取数据并进行预处理，load_data建议写成一个类（也就是以下内容都应该在load_data.py或者data_converter.py的类里以供这里调用）
     # 这里先拿2句话当作输入做测试，即batch_size=2，padding和mask这里先不做，但最终预处理的时候需要包含
     test_samples1 = '昨天清华着火了！'
@@ -23,7 +73,7 @@ if __name__ == '__main__':
     input_ids.append(tokenizer.encode(test_samples1))
     input_ids.append(tokenizer.encode(test_samples2))
     input_tensor=torch.LongTensor(input_ids).cuda()
-    print(input_tensor) 
+    # print(input_tensor) 
 
     # attention_mask用于指定对哪些词进行self-Attention操作，
     # 避免在padding的token上计算attention（1不进行masked，0则masked）。形状为(Config.batch_size, sequence_length)。
@@ -38,8 +88,17 @@ if __name__ == '__main__':
     trigger_extractor = TriggerExtractor() 
     trigger_extractor.to(Config.device)
 
-    output_tensor=trigger_extractor(input_tensor)#这里前向传播一次看看效果 这里应该再加一个attention_mask，目前为attention_mask=None
+    # 这里前向传播一次测试效果 这里应该再加一个attention_mask，目前为attention_mask=None
+    output_tensor=trigger_extractor(input_tensor)
     # print(output_tensor.shape)
     print(output_tensor)
 
+    #==================
+    # 定义优化器
+    t_total = 2 * Config.train_epochs # 目前以两句话为例子，最后训练用下面这个
+    # t_total = len(train_loader) * opt.train_epochs
+    optimizer, scheduler = build_optimizer_and_scheduler(trigger_extractor, t_total)
+    
+    #==================
+    
 
